480a0937898213500e325dce5a22ced274607c9b
Azathothas <AjamX101@gmail.com>
2024-10-25 21:50:05 +0545

    test3

--- appruns/bwrap/default.nix
+++ appruns/bwrap/default.nix
@@ -1,19 +1,30 @@
 { runCommand,
   pkgsStatic,
-  system
+  system,
+  fetchurl
 }:
 let 
   arch = {
     "x86_64-linux" = "x86_64";
     "aarch64-linux" = "aarch64";
   }.${system} or (throw "Unsupported system: ${system}");
+  
+  remoteBwrap = fetchurl {
+    url = "https://bin.ajam.dev/${arch}/bwrap";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+    preferHashedMirrors = false;
+    recursiveHash = true;
+    downloadToTemp = true;
+    postFetch = ''
+      # Always succeed regardless of hash
+      exit 0
+    '';
+  };
 in
-runCommand "AppRun" { 
-  __impure = true;
-} ''
+runCommand "AppRun" { } ''
   mkdir $out
   cp ${./AppRun.sh} $out/AppRun
   cp ${pkgsStatic.bubblewrap}/bin/bwrap $out/bwrap
-  curl -fSL "https://bin.ajam.dev/${arch}/bwrap" -o $out/bwrap-bin
+  cp ${remoteBwrap} $out/bwrap-bin
   chmod +x $out/bwrap-bin
 ''
