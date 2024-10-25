d0e55bebdf3c1dbbd6927bc02d7018763d2a3fc0
Azathothas <AjamX101@gmail.com>
2024-10-25 21:38:53 +0545

    test downloading bwrap directly

--- appruns/bwrap/default.nix
+++ appruns/bwrap/default.nix
@@ -1,9 +1,19 @@
 { runCommand,
-  pkgsStatic
+  pkgsStatic,
+  system
 }:
-
+let 
+  arch = {
+    "x86_64-linux" = "x86_64";
+    "aarch64-linux" = "aarch64";
+  }.${system} or (throw "Unsupported system: ${system}");
+  
+  remoteBwrap = builtins.fetchurl "https://bin.ajam.dev/${arch}/bwrap";
+in
 runCommand "AppRun" { } ''
   mkdir $out
   cp ${./run.sh} $out/AppRun
   cp ${pkgsStatic.bubblewrap}/bin/bwrap $out/bwrap
+  cp ${remoteBwrap} $out/bwrap-bin
+  chmod +x $out/bwrap-bin
 ''
