ec69f84b3ab1367d150b77799f74adab7474224d
Ajam <AjamX101@gmail.com>
2024-10-25 11:07:35 +0545

    +

--- appruns/bwrap/run.sh
+++ appruns/bwrap/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #LOCATION="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 LOCATION="$(dirname "$(realpath "$0")")"
-ENTRYPOINT="./$(readlink -f ${LOCATION}/entrypoint)"
+ENTRYPOINT="$(readlink -f ${LOCATION}/entrypoint)"
 BWRAP_BINDS="$(printf '%s ' $(printf '%s\n' /* | grep -v -E "dev|proc" | xargs -I % echo --bind % %))"
 eval "${LOCATION}/bwrap" "${BWRAP_BINDS}" --dev-bind "/dev" "/dev" --proc "/proc" --ro-bind "${LOCATION}/nix" "/nix" "${ENTRYPOINT}" "$@"
